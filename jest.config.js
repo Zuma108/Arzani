aa2292b1a074919bf5688241696f9f3ed0ed633f
ZumaTornado <michaaldekoya@gmail.com>
2025-05-01 19:14:09 +0100

    Arzani v.1.0.7

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+export default {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  testMatch: [
+    '**/__tests__/**/*.js',
+    '**/?(*.)+(spec|test).js'
+  ],
+  verbose: true,
+  forceExit: true,
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true
+};
